5625b7a4ac7b518fd1596feea94ea3fe737fb693
NazarStolyar <batinak94@gmail.com>
2020-02-15 20:03:02 +0200

    initial project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout>
+    <v-flex>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
---
+++ plugins/socket.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: false,
+  connection: 'http://metinseylan.com:1992',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
